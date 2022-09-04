eb582b32aabc337224d0a36ef3ef762c47e71470
sunyy520 <112803616+sunyy520@users.noreply.github.com>
2022-09-04 22:32:39 +0800

    Add files via upload

---
+++ src/main/java/cn/cvzhanshi/wechatpush/config/Pusher.java
@@ -0,0 +1,78 @@
+package cn.cvzhanshi.wechatpush.config;
+
+
+import cn.cvzhanshi.wechatpush.entity.Weather;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
+
+import java.util.Map;
+
+/**
+ * @author cVzhanshi
+ * @create 2022-08-04 21:09
+ */
+public class Pusher {
+
+    public static void main(String[] args) {
+        push();
+    }
+    private static String appId = "wxb6afd78bb063314e";
+    private static String secret = "4d0e3a34dd0819d44a4a0137b4850db0";
+
+
+
+    public static void push(){
+        //1，配置
+        WxMpInMemoryConfigStorage wxStorage = new WxMpInMemoryConfigStorage();
+        wxStorage.setAppId(appId);
+        wxStorage.setSecret(secret);
+        WxMpService wxMpService = new WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(wxStorage);
+        //2,推送消息
+        WxMpTemplateMessage templateMessage = WxMpTemplateMessage.builder()
+                .toUser("o7AUU5wnGWYKooMs-5vvwKBrDhFk")
+                .templateId("XAinrsf0RwrfpghYoUzIVNM-eJzWUx_YJmAaXVZrQaI")
+                .build();
+        //3,如果是正式版发送模版消息，这里需要配置你的信息
+        Weather weather = WeatherUtils.getWeather();
+        Map<String, String> map = CaiHongPiUtils.getEnsentence();
+        templateMessage.addData(new WxMpTemplateData("riqi",weather.getDate() + "  "+ weather.getWeek(),"#00BFFF"));
+        templateMessage.addData(new WxMpTemplateData("tianqi",weather.getText_now(),"#00FFFF"));
+        templateMessage.addData(new WxMpTemplateData("low",weather.getLow() + "","#173177"));
+        templateMessage.addData(new WxMpTemplateData("temp",weather.getTemp() + "","#EE212D"));
+        templateMessage.addData(new WxMpTemplateData("high",weather.getHigh()+ "","#FF6347" ));
+        templateMessage.addData(new WxMpTemplateData("windclass",weather.getWind_class()+ "","#42B857" ));
+        templateMessage.addData(new WxMpTemplateData("winddir",weather.getWind_dir()+ "","#B95EA3" ));
+        templateMessage.addData(new WxMpTemplateData("caihongpi",CaiHongPiUtils.getCaiHongPi(),"#FF69B4"));
+        templateMessage.addData(new WxMpTemplateData("lianai",JiNianRiUtils.getLianAi()+"","#FF1493"));
+        templateMessage.addData(new WxMpTemplateData("shengri1",JiNianRiUtils.getBirthday_Jo()+"","#FFA500"));
+        templateMessage.addData(new WxMpTemplateData("shengri2",JiNianRiUtils.getBirthday_Hui()+"","#FFA500"));
+
+        templateMessage.addData(new WxMpTemplateData("en",weather.getMtq() +"","#C71585"));
+        templateMessage.addData(new WxMpTemplateData("zh",weather.getMlow() +"度~~"+weather.getMhigh() +"度"+"","#C71585"));
+//        templateMessage.addData(new WxMpTemplateData("en",map.get("en") +"","#C71585"));
+//        templateMessage.addData(new WxMpTemplateData("zh",map.get("zh") +"","#C71585"));
+        String beizhu = "🌼今天是新的开始,元气满满🌼";
+        if(JiNianRiUtils.getLianAi() % 365 == 0){
+            beizhu = "你好呀,我们已经认识" + (JiNianRiUtils.getLianAi() / 365) + "周年啦！";
+        }
+        if(JiNianRiUtils.getBirthday_Jo()  == 0){
+            beizhu = "今天是你的生日，生日快乐呀！";
+        }
+        if(JiNianRiUtils.getBirthday_Hui()  == 0){
+            beizhu = "今天是新年啦,新年快乐呀！";
+        }
+        templateMessage.addData(new WxMpTemplateData("beizhu",beizhu,"#FF0000"));
+
+        try {
+            System.out.println(templateMessage.toJson());
+            System.out.println(wxMpService.getTemplateMsgService().sendTemplateMsg(templateMessage));
+        } catch (Exception e) {
+            System.out.println("推送失败：" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/cn/cvzhanshi/wechatpush/config/WeatherUtils.java
@@ -0,0 +1,48 @@
+package cn.cvzhanshi.wechatpush.config;
+
+import cn.cvzhanshi.wechatpush.entity.Weather;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author cVzhanshi
+ * @create 2022-08-04 22:02
+ */
+public class WeatherUtils {
+    public static void main(String[] args) {
+        System.out.println(getWeather());
+    }
+    public static Weather getWeather(){
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("district_id","320583"); // 昆山行政代码
+        map.put("data_type","all");//这个是数据类型
+        map.put("ak","UGshqk1Htprbejmhi1dK8XN0Ki903ytA");
+        String res = restTemplate.getForObject(
+                "https://api.map.baidu.com/weather/v1/?district_id={district_id}&data_type={data_type}&ak={ak}",
+                String.class,
+                map);
+        JSONObject json = JSONObject.parseObject(res);
+        System.out.println(res);
+        JSONArray forecasts = json.getJSONObject("result").getJSONArray("forecasts");
+        List<Weather> weathers = forecasts.toJavaList(Weather.class);
+        JSONObject now = json.getJSONObject("result").getJSONObject("now");
+        System.out.println(now);
+        Weather weather = weathers.get(0);
+        Weather mt = weathers.get(1);
+        weather.setText_now(now.getString("text"));
+        weather.setTemp(now.getString("temp"));
+        weather.setWind_class(now.getString("wind_class"));
+        weather.setWind_dir(now.getString("wind_dir"));
+
+        weather.setMhigh(mt.getHigh());
+        weather.setMlow(mt.getLow());
+        weather.setMtq(mt.getText_day());
+        return weather;
+    }
+}
